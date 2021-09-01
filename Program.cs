8156dc40e9574beaf5d4b0756e1107aae9c401ac
AnaComBia <anabeatrizetorres@gmail.com>
2021-09-01 12:11:00 -0300

    finaliza programa NomeCompleto

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NomeCompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\nescreva seu primeiro nome: ");
+            string nome = Console.ReadLine();
+            
+            Console.Write("escreva seu sobrenome: ");
+            string sobrenome = Console.ReadLine();
+
+            Console.WriteLine($"\nNome Completo: {nome} {sobrenome}");
+            Console.WriteLine($"Nome de Catálogo: {sobrenome.ToUpper()} {nome}\n");
+            
+            
+        }
+    }
+}
